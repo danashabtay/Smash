eec4bfd4a33579b307b2a439ef1068ac578ad8ec
danashabtay <99danashabtay@gmail.com>
2024-02-14 14:29:34 +0200

    m,,m

--- skeleton_smash/smash.cpp
+++ skeleton_smash/smash.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
 
     SmallShell& smash = SmallShell::getInstance();
     while(true) {
-        std::cout << this->getCurrPrompt(); /// changed to currPrompt;
+        std::cout << smash.getCurrPrompt(); /// changed to currPrompt;
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
